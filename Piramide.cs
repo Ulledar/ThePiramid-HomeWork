f396ff302ecb19679b3eaf91564ff788d2cb4eab
Alex G <ulledar@ua.fm>
2021-05-14 15:06:40 +0300

    little upd

--- Piramide.cs
+++ Piramide.cs
@@ -12,7 +12,7 @@ namespace ThePiramide
 
             for (int level = 0; level < rows*2; level+=2)
             {
-                for (int i = 0; i < maxStars-level; i++)
+                for (int i = 0; i <= maxStars-level; i++)
                 {
                     Write(" ");
                 }
