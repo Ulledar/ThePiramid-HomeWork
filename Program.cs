b0bf601464d1ec49e4240ff8a8d6d799be873b03
Alex G <ulledar@ua.fm>
2021-05-14 11:26:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace ThePiramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 7;
+            int maxStars = 13;
+
+            for (int level = 0; level < rows*2; level+=2)
+            {
+                for (int i = 0; i < maxStars-level; i++)
+                {
+                    Write(" ");
+                }
+                for (int i = 0; i < level; i++)
+                {
+                    Write("* ");
+                }
+                WriteLine("*");
+            }
+        }
+    }
+}
